baf757924672082d9b5c1dae8194250c92001d18
blablupcom <olegvolodin@eyahoo.com>
2015-11-25 21:48:57 +0300

    first commit

--- scraper.py
+++ scraper.py
@@ -44,7 +44,6 @@ def validateURL(url):
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
             r = urllib2.urlopen(url)
-
         sourceFilename = r.headers.get('Content-Disposition')
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
